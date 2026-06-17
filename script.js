177909824d8b55dec3902b29937205fcc71d4918
CHRIS THE FLAWLESS WIZARD <chrismukhwana456@gmail.com>
2026-06-17 10:41:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-function addValue(value){
-    document.getElementById("display").value += value;
+let display = document.getElementById("display");
+
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+button.onclick = () => {
+
+    let value = button.innerText;
+
+    if(value=="C"){
+        display.innerText="0";
+    }
+
+    else if(value=="="){
+        try{
+            display.innerText = eval(display.innerText);
+        }
+        catch{
+            display.innerText="Error";
+        }
+    }
+
+    else{
+        if(display.innerText=="0")
+            display.innerText=value;
+        else
+            display.innerText += value;
+    }
+
 }
 
-
-function clearDisplay(){
-    document.getElementById("display").value="";
-}
-
-
-function calculate(){
-    let result = eval(document.getElementById("display").value);
-    document.getElementById("display").value=result;
-}
+});
