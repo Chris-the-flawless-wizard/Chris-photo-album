1187826530ab4d955f80d4af8883495e0f64ecd9
CHRIS THE FLAWLESS WIZARD <chrismukhwana456@gmail.com>
2026-06-17 10:32:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function addValue(value){
+    document.getElementById("display").value += value;
+}
 
+
+function clearDisplay(){
+    document.getElementById("display").value="";
+}
+
+
+function calculate(){
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value=result;
+}
